fde7ae4e71830653915c89a014c1e49377f99321
kan <root.kan.ss@gmail.com>
2021-05-06 22:45:13 -0400

    Fix redudant code

--- Src/NikkiMusic/Main.cs
+++ Src/NikkiMusic/Main.cs
@@ -1,24 +1,21 @@
-using System;
 using System.Collections.Generic;
 using GDMechanic.Wiring;
 using GDMechanic.Wiring.Attributes;
-using GDMechanic.Extensions;
 using Godot;
-using NikkiMusic.Actions;
-using NikkiMusic.Systems;
+
 using NikkiMusic.UI;
 
 namespace NikkiMusic
 {
 	public class Main : Node
 	{
-		[Child()] private Menu menu;
+		[Child] private Menu menu;
 		
-		private List<object> Canciones = new List<object>(){new object(), new object(), new object()};
+		private readonly List<object> canciones = new List<object>{new object(), new object(), new object()};
 		public override void _Ready()
 		{
 			this.Wire();
-			menu.Init(Canciones); 
+			menu.Init(canciones); 
 		}
 
 		
--- Src/NikkiMusic/UI/Menu.cs
+++ Src/NikkiMusic/UI/Menu.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using Godot;
 using GDMechanic.Wiring;
 using GDMechanic.Wiring.Attributes;
-using GDMechanic.Extensions;
 
 namespace NikkiMusic.UI
 {
